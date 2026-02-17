ab49e4c89def3f3e646e45b8e6d7a450d311df9c
retroarab <iosifdala@yahoo.com>
2026-02-17 16:45:17 +0200

    remove

--- book/learning_common_collections/target/debug/deps/learning_common_collections-03e3ebfa51598366.d
+++
@@ -1,9 +0,0 @@
-/home/ezgad/Desktop/RUST/projects/book/learning_common_collections/target/debug/deps/learning_common_collections-03e3ebfa51598366.d: src/main.rs Cargo.toml
-
-/home/ezgad/Desktop/RUST/projects/book/learning_common_collections/target/debug/deps/liblearning_common_collections-03e3ebfa51598366.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
