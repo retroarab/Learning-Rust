67f697c44b315f20f4e7907e95d228cded835957
retroarab <iosifdala@yahoo.com>
2026-02-21 23:38:43 +0200

    2 new endpoints, one for json hanlding and one to get user and return in json

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,7 +1,25 @@
-use sqlx::{self, Error, SqlitePool};
+use std::fmt::format;
+
+use axum::{Json, extract::State, http::StatusCode};
+use serde_json::{Value, json};
+use sqlx::{self, Error, Row, SqlitePool, pool};
 pub async fn init_db() -> Result<SqlitePool, Error> {
     let database_url = std::env::var("DATABASE_URL").expect("Database url must be set");
     let connection_pool = SqlitePool::connect(&database_url).await?;
     sqlx::migrate!("./migrations").run(&connection_pool).await?;
     Ok(connection_pool)
+}
+pub async fn get_user(State(pool): State<SqlitePool>) -> Result<Json<Value>, StatusCode> {
+    let rows = sqlx::query("select * from yuseftest")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let mut json_out: String = String::new();
+    for row in &rows {
+        let id: i64 = row.get("id");
+        let name: String = row.get("name");
+        println!("{}-{}", id, name);
+        json_out = String::from(format!("{id}-{name}"))
+    }
+    Ok(Json(json!({ "rows": rows.len(),"user:": json_out })))
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,23 @@
 mod db;
-use crate::db::init_db;
-use axum::{Router, routing::get};
-use sqlx::{SqlitePool, error::BoxDynError};
+use crate::db::{get_user, init_db};
+use axum::{Json, Router, routing::get};
+use sqlx::{Error, SqlitePool, error::BoxDynError};
 use tokio::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
     let connection_pool = init_db().await?;
-    let app = Router::new().route("/", get(greet_user));
+    let app = Router::new()
+        .route("/", get(greet_user))
+        .route("/user", get(get_user))
+        .route("/json", get(testing_json))
+        .with_state(connection_pool);
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await?;
     Ok(())
+}
+async fn testing_json() -> Json<String> {
+    Json("Muie".to_string())
 }
 
 async fn greet_user() -> String {
